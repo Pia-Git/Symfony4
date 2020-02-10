f5693b1d0192b099780df26d7c18bb45af20f9f3
Pia-Git <p.c.sinzig@gmail.com>
2020-02-10 19:12:34 +0100

    Create first Admin (CategoryAdmin)
    * create 2 database models (Category, BlogPost)
    * create CategoryAdmin.php
    * add CategoryAdmin to services.yaml
    * set up database (local mysql server)

---
+++ sym4/src/Entity/BlogPost.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity;
+
+class BlogPost
+{
+    /**
+    * @var string
+    *
+    * @ORM\Column(name="title", type="string")
+    */
+    private $title;
+
+    /**
+    * @var string
+    *
+    * @ORM\Column(name="body", type="text")
+    */
+    private $body;
+
+    /**
+    * @var bool
+    *
+    * @ORM\Column(name="draft", type="boolean")
+    */
+    private $draft = false;
+
+    /**
+    * @ORM\ManyToOne(targetEntity="Category", inversedBy="blogPosts")
+    */
+    private $category;
+}
